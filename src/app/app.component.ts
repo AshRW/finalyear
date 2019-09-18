d9502312aff5432fad51c4ae0eb2a35882a8b068
AshRW <ashrwankhede@gmail.com>
2019-09-18 20:24:51 +0530

    added new component: home_page and firebase json backuped

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(){
+    console.log("Hello world");
+  }
   title = 'mainproject';
 }
+
+
+// var snapshotToArray = function(snaps){
+//   var returnArr = [];
+//   snaps.forEach(element => {
+//     let key = element.key;
+//     let obj = element.payload.val();
+//     obj.key = key;
+//     returnArr.push(obj);
+//   });
+//   return returnArr;
+// }
