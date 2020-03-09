7dcfba4a355c79589de114f8ab3ec4d0f20f573e
AshRW <ashrwankhede@gmail.com>
2020-03-09 20:21:35 +0530

    09 March 2020 Moodle folder inside done File disply, back button and uploadding yet to be done

--- src/app/services/fireser.service.ts
+++ src/app/services/fireser.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth/';
 import * as firebase from 'firebase';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { BASE_URL } from '../base_url';
 
 @Injectable({
   providedIn: 'root'
@@ -14,13 +15,23 @@ export class FireserService {
     return this.afd.object(path).set(data);
   }
   test(){
-    console.log("test success");
+    console.log("Fireser Service Works!");
   }
   pullList(path){
-    return this.afd.list(path);
+    return this.afd.list(BASE_URL+path);
   }
   pullObject(path){
-    return this.afd.object(path);
+    return this.afd.object(BASE_URL+path);
+  }
+  snapshotToArray2(snaps){
+    var returnArr = [];
+    snaps.forEach(element => {
+      let key = element.key;
+      let obj = element.payload.val();
+      obj.key = key;
+      returnArr.push(obj);
+    });
+    return returnArr;
   }
 }
 
